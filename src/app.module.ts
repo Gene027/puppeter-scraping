a77c242b01f8a8840dbeb5086007007c25f56cb2
gene027 <anosikepraise@gmail.com>
2024-04-03 12:35:50 +0100

    reset to default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,8 @@
 import { Module } from '@nestjs/common';
-import { WinstonModule } from 'nest-winston';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configuration, dataSourceOptions } from './config';
-import { KeepaModule } from './keepa/keepa.module';
-import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ScrapingModule } from './scrapper/scraping.module';
 
 @Module({
-  imports: [
-    WinstonModule.forRoot(configuration().logging),
-    TypeOrmModule.forRoot(dataSourceOptions),
-    ScheduleModule.forRoot(),
-    KeepaModule,
-    ScrapingModule,
-  ],
   controllers: [AppController],
   providers: [AppService],
 })
